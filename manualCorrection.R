69f44203ea4a67d374d2cfd7bc3af1d92e0a4586
mitchelkappen <36090493+mitchelkappen@users.noreply.github.com>
2021-12-01 14:51:55 +0100

    Create manualCorrection.R

---
+++ manualCorrection.R
@@ -0,0 +1,31 @@
+require(graphics)
+
+set.seed(123)
+x <- rnorm(25, mean = c(rep(0, 25), rep(3, 25)))
+p <- 2*pnorm(sort(-abs(x)))
+
+round(p, 3)
+round(p.adjust(p), 3)
+round(p.adjust(p, "BH"), 3)
+
+## or all of them at once (dropping the "fdr" alias):
+# p.adjust.M <- p.adjust.methods[p.adjust.methods != "fdr"]
+p.adjust.M <- p.adjust.methods
+p.adj    <- sapply(p.adjust.M, function(meth) p.adjust(p, meth))
+p.adj.60 <- sapply(p.adjust.M, function(meth) p.adjust(p, meth, n = length(p)))
+stopifnot(identical(p.adj[,"none"], p), p.adj <= p.adj.60)
+round(p.adj, 3)
+## or a bit nicer:
+noquote(apply(p.adj, 2, format.pval, digits = 3))
+
+
+## and a graphic:
+matplot(p, p.adj.60, ylab="p.adjust(p, meth)", type = "l", asp = 1, lty = 1:6,
+        main = "P-value adjustments")
+legend(0.7, 0.6, p.adjust.M, col = 1:6, lty = 1:6)
+
+## Can work with NA's:
+pN <- p; iN <- c(46, 47); pN[iN] <- NA
+pN.a <- sapply(p.adjust.M, function(meth) p.adjust(pN, meth))
+## The smallest 20 P-values all affected by the NA's :
+round((pN.a / p.adj)[1:20, ] , 4)
